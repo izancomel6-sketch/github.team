ed62721a034fed0dcda826d034bbe87d6c780cf3
SK MAWAR NAVIGATOR APLICATION <izancomel6@gmail.com>
2026-07-30 09:23:56 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
-function openMap() {
-    document.getElementById("homePage").style.display = "none";
-    document.getElementById("mapPage").classList.remove("hidden");
-    document.getElementById("mapPage").style.display = "block";
-}
+const home = document.getElementById("home");
+const map = document.getElementById("map");
 
-function goHome() {
-    document.getElementById("mapPage").classList.add("hidden");
-    document.getElementById("mapPage").style.display = "none";
-    document.getElementById("homePage").style.display = "flex";
-}
+document.getElementById("startBtn").addEventListener("click", function(){
 
-function showPlace(place) {
-    alert("Anda memilih: " + place);
-}
+home.style.display = "none";
+
+map.style.display = "block";
+
+});
+
+document.getElementById("backBtn").addEventListener("click", function(){
+
+map.style.display = "none";
+
+home.style.display = "flex";
+
+});
+
+document.querySelector(".pejabat").addEventListener("click", function(){
+
+alert("Anda memilih Pejabat");
+
+});
+
+document.querySelector(".tandas").addEventListener("click", function(){
+
+alert("Anda memilih Tandas");
+
+});
+
+document.querySelector(".surau").addEventListener("click", function(){
+
+alert("Anda memilih Surau");
+
+});
+
+document.querySelector(".bilik").addEventListener("click", function(){
+
+alert("Anda memilih Bilik Mesyuarat");
+
+});
