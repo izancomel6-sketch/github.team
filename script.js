c459ee9ea979b26011ecf39ff46281415dd1fadf
SK MAWAR NAVIGATOR APLICATION <izancomel6@gmail.com>
2026-07-30 09:11:49 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function openMap(){
 
+document.getElementById("homePage").style.display="none";
+
+document.getElementById("mapPage").style.display="block";
+
+}
+
+function goHome(){
+
+document.getElementById("mapPage").style.display="none";
+
+document.getElementById("homePage").style.display="flex";
+
+}
+
+function showPlace(place){
+
+alert("Anda memilih : " + place);
+
+}
